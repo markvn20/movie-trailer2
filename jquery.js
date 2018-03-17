ced9cf8c27987b8854570c68f7e6d42c1f758990
markvn20 <mnguye17@asu.edu>
2018-03-17 11:58:03 -0700

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -8,27 +8,28 @@ function apiCall(api, api2) {
         success: function(data) {
             var resultsLength = data.results.length;
 			var someObject;
+			
 			for(var i = 0; i < resultsLength; i++) {
-				var resultsShort = data.results[i];
+				var resultsShort 	= data.results[i];
+				var image 			= resultsShort.poster_path;
+				var movieId 		= resultsShort.id;
 				please = data.results[i].id;
 				someObject = {
 				    voteCount: resultsShort.vote_count,
 				    id: resultsShort.id
 				};
 				var okay = data.results[i].id;
-				test(someObject.id)
-				//test2(someObject.id)
+				$('.container').append('<div class="asdf" movieId="'+movieId+'"><img src="http://image.tmdb.org/t/p/w185/'+image+'"></div>')
+			
 			}
-			function test(par1, par2) {
+			function test(par1) {
 			 	$.ajax({
 	                url: 'https://api.themoviedb.org/3/movie/'+par1+'?api_key=ece7966c119923d24c65ccb57a5da71c&language=en-US&page=1',
 	                success: function (data) {
-	                	var 
-	                	var results2Id = data.id
-	                	console.log(data)
-	                	var poster = data.poster_path;
-	                	$('.container').append('<div class="asdf"><img src="http://image.tmdb.org/t/p/w185/'+poster+'"></div>')
-	                }
+	                	var results2Id = data.id;
+	                	
+	                	
+                	}
 	            });
 	        }
 		     
@@ -37,22 +38,26 @@ function apiCall(api, api2) {
 	                url: 'https://api.themoviedb.org/3/movie/'+id+'/videos?api_key=ece7966c119923d24c65ccb57a5da71c&language=en-US',
 	                success: function(data2) {
 	                	var resultsKey = data2.results.length;
-	                	for(var j = 0; j < resultsKey; j++) {
-	                		if(data2.results[j].type = 'trailer') {
-	                			var h = data2.results[j].key;
-	             				
-	                		}
-
-	                	}
-
+	                	var results2Array = data2.results;
+	                	var z = -1;
+	                	var v = 0;
+	                	for (var prop in results2Array) {
+						    results2Array[prop] = 'teaser';
+						    console.log(results2Array[prop])
+						}
+					  	console.log(v)
+	                	$('iframe').attr('src',  'https://www.youtube.com/embed/' + get_movieKey);
 	                }
 	            });
 	        }	
            	$(document).on('click', 'div.asdf', function() {
-				var o = $(this).attr("data");
-				test(null, 'original')
-				$('iframe').attr('src',  'https://www.youtube.com/embed/' + o);
+				var get_movieKey = $(this).attr("data");
+				var get_movieID = $(this).attr('movieId');
+				test(get_movieID);
+				test2(get_movieID);
+				
 			})
+
         }
     });
    
