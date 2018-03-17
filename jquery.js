94a3ccfc803167de3038efc5d76665bfd87fc6b6
markvn20 <mnguye17@asu.edu>
2018-03-17 06:10:39 -0700

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {  
+
 var arrayData2 = [];
 function apiCall(api, api2) {
 	var please;
@@ -14,37 +16,47 @@ function apiCall(api, api2) {
 				    id: resultsShort.id
 				};
 				var okay = data.results[i].id;
-		       	test(someObject.id)
-				function test (par1) {
-				 	$.ajax({
-		                url: 'https://api.themoviedb.org/3/movie/'+par1+'?api_key=ece7966c119923d24c65ccb57a5da71c&language=en-US&page=1',
-		                success: function (data) {
-		                	var results2Id = data.id
-	                	 	
-
-	                	 	test2(results2Id)
-							function test2 (id) {
-							 	$.ajax({
-					                url: 'https://api.themoviedb.org/3/movie/'+id+'/videos?api_key=ece7966c119923d24c65ccb57a5da71c&language=en-US',
-					                success: function(data2) {
-					                	var resultsKey = data2.results.length;
-					               		console.log(data2)
-					                	for(var j = 0; j < resultsKey; j++) {
-					                		if(data2.results[j].type = 'trailer') {
-					                			return data2.results[j].type = 'trailer';
-					                		}
-					                		
-					                	}
-					                }
-					            });
-					        }	
-		                }
-		            });
-		        }
+				test(someObject.id)
+				//test2(someObject.id)
 			}
-           
+			function test(par1, par2) {
+			 	$.ajax({
+	                url: 'https://api.themoviedb.org/3/movie/'+par1+'?api_key=ece7966c119923d24c65ccb57a5da71c&language=en-US&page=1',
+	                success: function (data) {
+	                	var 
+	                	var results2Id = data.id
+	                	console.log(data)
+	                	var poster = data.poster_path;
+	                	$('.container').append('<div class="asdf"><img src="http://image.tmdb.org/t/p/w185/'+poster+'"></div>')
+	                }
+	            });
+	        }
+		     
+			function test2(id) {
+			 	$.ajax({
+	                url: 'https://api.themoviedb.org/3/movie/'+id+'/videos?api_key=ece7966c119923d24c65ccb57a5da71c&language=en-US',
+	                success: function(data2) {
+	                	var resultsKey = data2.results.length;
+	                	for(var j = 0; j < resultsKey; j++) {
+	                		if(data2.results[j].type = 'trailer') {
+	                			var h = data2.results[j].key;
+	             				
+	                		}
+
+	                	}
+
+	                }
+	            });
+	        }	
+           	$(document).on('click', 'div.asdf', function() {
+				var o = $(this).attr("data");
+				test(null, 'original')
+				$('iframe').attr('src',  'https://www.youtube.com/embed/' + o);
+			})
         }
     });
+   
 }
 
 apiCall('popular')
+});
